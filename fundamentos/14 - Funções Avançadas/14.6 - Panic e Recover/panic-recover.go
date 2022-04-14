fed4b226b5ff674396e1a1f127602782eeedb0dd
Alexandre Bueno da Silva <abueno.silva@gmail.com>
2022-04-14 09:44:48 -0300

    Atualização

--- fundamentos/14 - Funções Avançadas/14.6 - Panic e Recover/panic-recover.go
+++ fundamentos/14 - Funções Avançadas/14.6 - Panic e Recover/panic-recover.go
@@ -1,9 +1,30 @@
 package main
 
+import "fmt"
+
+func recuperarExecucao() {
+	if r := recover(); r != nil {
+		fmt.Println("Recovery executado com sucesso")
+	}
+}
+
 func alunoEstaAprovado(n1, n2 float32) bool {
 	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+
+	panic("A MÉDIA É EXATAMENTE 6")
 }
 
 func main() {
+	defer recuperarExecucao()
+	fmt.Println(alunoEstaAprovado(6, 7))
+	fmt.Println(alunoEstaAprovado(3, 7))
+	fmt.Println(alunoEstaAprovado(6, 6))
+	fmt.Println("Pós execução")
 
 }

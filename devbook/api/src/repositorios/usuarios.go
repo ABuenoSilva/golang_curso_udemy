6bd2a4140196d26bfa772b2a8edee2d786a4782b
Alexandre Bueno da Silva <abueno.silva@gmail.com>
2022-04-17 16:05:22 -0300

    Novas aulas

--- devbook/api/src/repositorios/usuarios.go
+++ devbook/api/src/repositorios/usuarios.go
@@ -168,6 +168,115 @@ func (repositorio usuarios) Seguir(usuarioID, seguidorID uint64) error {
 	return nil
 }
 
+func (repositorio usuarios) PararDeSeguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from seguidores where usuario_id =  ? and seguidor_id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (repositorio usuarios) BuscarSeguidores(usuarioID uint64) ([]modelos.Usuario, error) {
+
+	linhas, erro := repositorio.db.Query(
+		`select id, u.nome, u.nick, u.email, u.criadoEm 
+		 from usuarios u inner join seguidores s 
+		 on u.id = s.seguidor_id and s.usuario_id = ?`,
+		usuarioID)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var seguidores []modelos.Usuario
+	for linhas.Next() {
+		var seguidor modelos.Usuario
+		if erro = linhas.Scan(
+			&seguidor.ID,
+			&seguidor.Nome,
+			&seguidor.Nick,
+			&seguidor.Email,
+			&seguidor.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+		seguidores = append(seguidores, seguidor)
+	}
+
+	return seguidores, nil
+
+}
+
+func (repositorio usuarios) BuscarSeguindo(usuarioID uint64) ([]modelos.Usuario, error) {
+
+	linhas, erro := repositorio.db.Query(
+		"select id, u.nome, u.nick, u.email, u.criadoEm from usuarios u inner join seguidores s on u.id = s.usuario_id and s.seguidor_id = ?", usuarioID)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var seguidores []modelos.Usuario
+	for linhas.Next() {
+		var seguidor modelos.Usuario
+		if erro = linhas.Scan(
+			&seguidor.ID,
+			&seguidor.Nome,
+			&seguidor.Nick,
+			&seguidor.Email,
+			&seguidor.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+		seguidores = append(seguidores, seguidor)
+	}
+
+	return seguidores, nil
+
+}
+
+func (repositorio usuarios) BuscarSenha(usuarioID uint64) (string, error) {
+	linhas, erro := repositorio.db.Query("select senha from usuarios where id = ?", usuarioID)
+	if erro != nil {
+		return "", erro
+	}
+	defer linhas.Close()
+
+	var usuario modelos.Usuario
+
+	if linhas.Next() {
+		if erro = linhas.Scan(
+			&usuario.Senha,
+		); erro != nil {
+			return "", erro
+		}
+	} else {
+		return "", errors.New("o id enviado não foi encontrado no banco de dados")
+	}
+
+	return usuario.Senha, nil
+}
+
+func (repositorio usuarios) AtualizarSenha(usuarioID uint64, novaSenha string) error {
+	statement, erro := repositorio.db.Prepare("update usuarios set senha = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro := statement.Exec(novaSenha, usuarioID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
 func NovoRepositorioDeUsuarios(db *sql.DB) *usuarios {
 	return &usuarios{db}
 }
